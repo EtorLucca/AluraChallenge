13544d1724a82494f03db72848f3c89d7c91251a
Etor Lucca <etor.lucca78@gmail.com>
2021-12-24 13:03:04 -0300

    Seleção de cor de fundo do projeto adicionada.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 function editor(){
-    window.location.href = "./index.html"
+    window.location.href = "./index.html";
 }
 
 function comunidade(){
-    window.location.href = "./comunidade.html"
+    window.location.href = "./comunidade.html";
+}
+
+function ReturnColor(c){
+    var c = document.getElementById("color").value;
+    let codeEditor = document.getElementById('code__editor');
+    codeEditor.style.backgroundColor = c   
 }
