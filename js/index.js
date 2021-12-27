dfcc282a8830236ce9b26f32525b3124d07da1cb
Etor Lucca <etor.lucca78@gmail.com>
2021-12-27 08:49:29 -0300

    Implementação do highlight.js

--- js/index.js
+++ js/index.js
@@ -11,3 +11,18 @@ function ReturnColor(c){
     let codeEditor = document.getElementById('code__editor');
     codeEditor.style.backgroundColor = c   
 }
+
+const linguagem = document.querySelector('.project__language');
+const areaCodigo = document.querySelector('.code__wrapper');
+const botao = document.querySelector('.main__button');
+
+function aplicaHighlight() {
+    const codigo = areaCodigo.innerText;
+    areaCodigo.innerHTML = `<code class="text__editor hljs ${linguagem.value}" contenteditable="true" aria-label="Editor de código"></code>`;
+    areaCodigo.querySelector('code').textContent = codigo;
+    hljs.highlightElement(areaCodigo.querySelector('code'));
+}
+
+botao.addEventListener('click', () => {
+    aplicaHighlight();
+})
